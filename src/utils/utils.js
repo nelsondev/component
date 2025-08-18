e5b7e53b5a18f20e5f193c09df214ed7af840cab
Nelson M <nelsontron@outlook.com>
2025-08-18 10:17:10 -0600

    fix camelToKebab

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,4 +1,4 @@
-export const camelToKebab = str => str.replace(/-([a-z])/g, (_, letter) => letter.toUpperCase())
+export const camelToKebab = str => str.replace(/([a-z0-9])([A-Z])/g, '$1-$2').toLowerCase();
 
 export const kebabToCamel = str => str.replace(/-([a-z])/g, (_, letter) => letter.toUpperCase())
 
