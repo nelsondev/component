48201d96b9f9977f7fb96bc6e32369ca29510051
Nelson M <nelsontron@outlook.com>
2025-08-19 09:47:30 -0600

    syntax sugar for VSCode extensions

--- src/index.js
+++ src/index.js
@@ -5,10 +5,15 @@
 
 import { defineComponent } from './core/component.js'
 
+const html = String.raw;
+const template = html;
+
 // Expose globally for script tag usage
 if (typeof window !== 'undefined') {
     window.defineComponent = defineComponent;
     window.TronComponent = { defineComponent };
+    window.html = html;
+    window.template = template;
 }
 
-export { defineComponent }
+export { defineComponent, html, template }
